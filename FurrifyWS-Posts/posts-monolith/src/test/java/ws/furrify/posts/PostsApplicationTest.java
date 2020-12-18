91bdbc71aabf9af42a4e8db767aa5693e3a4b5aa
Skyterix1991 <skyterix1991@gmail.com>
2020-12-18 21:28:21 +0100

    Basic config

---
+++ FurrifyWS-Posts/posts-monolith/src/test/java/ws/furrify/posts/PostsApplicationTest.java
@@ -0,0 +1,13 @@
+package ws.furrify.posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ FurrifyWS-Posts/posts-shared/src/main/java/ws/furrify/posts/exception/RecordAlreadyExistsException.java
@@ -0,0 +1,17 @@
+package ws.furrify.posts.exception;
+
+import lombok.Getter;
+
+/**
+ * @author Skyte
+ */
+public class RecordAlreadyExistsException extends RuntimeException implements RestException {
+
+    @Getter
+    private final HttpStatus status = HttpStatus.CONFLICT;
+
+    public RecordAlreadyExistsException(String message) {
+        super(message);
+    }
+
+}
