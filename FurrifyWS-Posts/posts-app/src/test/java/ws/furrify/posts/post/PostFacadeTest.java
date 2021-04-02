d43dac64b11390a3df769860e90542f0eb27cd6d
Sky <skyterix1991@gmail.com>
2021-04-02 12:42:56 +0200

    Fix naming refactor

--- FurrifyWS-Posts/posts-app/src/test/java/ws/furrify/posts/post/PostFacadeTest.java
+++ FurrifyWS-Posts/posts-app/src/test/java/ws/furrify/posts/post/PostFacadeTest.java
@@ -26,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class MediaFacadeTest {
+class PostFacadeTest {
 
     private static PostRepository postRepository;
     private static PostFacade postFacade;
