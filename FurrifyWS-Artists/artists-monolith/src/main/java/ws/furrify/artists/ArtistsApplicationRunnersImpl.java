4c1c4243e7b8831945b4095ccfcdbd17f9878edc
Sky <skyterix1991@gmail.com>
2021-05-12 13:09:49 +0200

    Kafka consumer regions added

---
+++ FurrifyWS-Artists/artists-monolith/src/main/java/ws/furrify/artists/ArtistsApplicationRunnersImpl.java
@@ -0,0 +1,13 @@
+package ws.furrify.artists;
+
+import org.springframework.stereotype.Component;
+import ws.furrify.shared.runner.CheckRegionEnvCommandLineRunner;
+
+/**
+ * Class implementing runners from shared module.
+ *
+ * @author sky
+ */
+@Component
+class ArtistsApplicationRunnersImpl extends CheckRegionEnvCommandLineRunner {
+}
---
+++ FurrifyWS-Posts/posts-monolith/src/main/java/ws/furrify/posts/PostsApplicationRunnersImpl.java
@@ -0,0 +1,13 @@
+package ws.furrify.posts;
+
+import org.springframework.stereotype.Component;
+import ws.furrify.shared.runner.CheckRegionEnvCommandLineRunner;
+
+/**
+ * Class implementing runners from shared module.
+ *
+ * @author sky
+ */
+@Component
+class PostsApplicationRunnersImpl extends CheckRegionEnvCommandLineRunner {
+}
