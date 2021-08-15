473d7e463174527f1dffcac8f65c04513b7e2e15
Sky <skyterix1991@gmail.com>
2021-08-15 23:53:11 +0200

    Microservice names in exception messages

--- FurrifyWS-Shared/src/main/java/ws/furrify/shared/exception/Errors.java
+++ FurrifyWS-Shared/src/main/java/ws/furrify/shared/exception/Errors.java
@@ -25,9 +25,9 @@ public enum Errors {
     RECORD_ALREADY_EXISTS("Record [uuid={0}] already exists."),
     TAG_ALREADY_EXISTS("Tag [value={0}] already exists."),
     BAD_REQUEST("Given request data is invalid."),
-    CHAIN_OF_REQUESTS_BROKEN("Chain of requests was broken with exception."),
-    CHAIN_OF_REQUESTS_UNAUTHORIZED("Chain of requests was broken with unauthorized call. " +
-            "Make sure you have all scopes required for this request."),
+    CHAIN_OF_REQUESTS_BROKEN("There was an exception when contacting [name={0}] microservice."),
+    CHAIN_OF_REQUESTS_UNAUTHORIZED("Chain of requests was broken with unauthorized call to [name={0}] microservice. " +
+            "Make sure you have all roles required for this request."),
     PREFERRED_NICKNAME_IS_NOT_VALID("Preferred nickname [nickname={0}] is not in nicknames array."),
     PREFERRED_NICKNAME_IS_TAKEN("Preferred nickname [nickname={0}] is already taken."),
     NICKNAMES_CANNOT_BE_EMPTY("Nicknames array must not be empty."),
