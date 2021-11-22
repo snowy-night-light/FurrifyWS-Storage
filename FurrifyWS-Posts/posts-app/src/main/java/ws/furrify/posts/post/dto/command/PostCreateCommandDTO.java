3b6da0b59fabccb2a683a5a275b26083d5ba722b
Sky <skyterix1991@gmail.com>
2021-11-22 15:36:32 +0100

    Desc fix

--- FurrifyWS-Posts/posts-app/src/main/java/ws/furrify/posts/post/dto/command/PostCreateCommandDTO.java
+++ FurrifyWS-Posts/posts-app/src/main/java/ws/furrify/posts/post/dto/command/PostCreateCommandDTO.java
@@ -1,5 +1,6 @@
 package ws.furrify.posts.post.dto.command;
 
+import lombok.NonNull;
 import lombok.ToString;
 import lombok.Value;
 import ws.furrify.posts.post.dto.PostDTO;
@@ -25,7 +26,8 @@ public class PostCreateCommandDTO implements CommandDTO<PostDTO> {
     @Size(min = 1, max = 64)
     String title;
 
-    @Size(min = 1, max = 1024)
+    @Size(max = 1024)
+    @NonNull
     String description;
 
     @NotNull
