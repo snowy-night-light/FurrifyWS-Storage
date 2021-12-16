70750f3d950644c93ea9b574ea203e9e0fae1071
Sky <skyterix1991@gmail.com>
2021-12-16 15:11:33 +0100

    Legacy fix

--- FurrifyWS-Posts/posts-adapters/src/main/java/ws/furrify/posts/security/SecurityConfig.java
+++ FurrifyWS-Posts/posts-adapters/src/main/java/ws/furrify/posts/security/SecurityConfig.java
@@ -1,42 +1,22 @@
 package ws.furrify.posts.security;
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ws.furrify.shared.security.OpenFeignKeycloakInterceptor;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter implements WebMvcConfigurer {
-    @Autowired
-    public void configureGlobal(
-            AuthenticationManagerBuilder auth) {
+class SecurityConfig {
 
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider
-                = keycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
-                new SimpleAuthorityMapper());
-        auth.authenticationProvider(keycloakAuthenticationProvider);
+    /**
+     * Needs to be here, for whatever reason it cannot be placed in KeycloakConfig class cause framework doesn't like it.
+     */
+    @Bean
+    KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
     }
 
     @Bean
@@ -45,54 +25,8 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter implements Web
     }
 
     @Bean
-    KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
-    }
-
-    @Bean
     OpenFeignKeycloakInterceptor openFeignKeycloakInterceptor() {
         return new OpenFeignKeycloakInterceptor();
     }
 
-    @Bean
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(
-                new SessionRegistryImpl());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.authorizeRequests()
-                .anyRequest().hasRole("user");
-
-        http.csrf().disable();
-
-        http.cors()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "PATCH", "DELETE");
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
-        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE"));
-        configuration.setAllowCredentials(false);
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "Accept"));
-        configuration.setExposedHeaders(Arrays.asList("Authorization", "Id", "Expires"));
-
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
 }
