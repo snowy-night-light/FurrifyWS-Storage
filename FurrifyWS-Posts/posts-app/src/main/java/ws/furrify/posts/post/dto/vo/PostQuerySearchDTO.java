2b1216fbadd275e48146d21aa13937b3348553ae
Sky <skyterix1991@gmail.com>
2022-01-12 00:18:02 +0100

    Search by title

--- FurrifyWS-Posts/posts-app/src/main/java/ws/furrify/posts/post/dto/vo/PostQuerySearchDTO.java
+++ FurrifyWS-Posts/posts-app/src/main/java/ws/furrify/posts/post/dto/vo/PostQuerySearchDTO.java
@@ -8,6 +8,7 @@ import lombok.Value;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
@@ -22,11 +23,15 @@ import java.util.stream.Collectors;
 @ToString
 public class PostQuerySearchDTO implements Serializable {
 
+    private final static Pattern WORDS_PATTERN = Pattern.compile("[A-Za-z][A-Za-z0-9]*");
+
     private final static String WITH_ARTIST_SYMBOL = "@";
     private final static String WITHOUT_ARTIST_SYMBOL = "-@";
 
     private final static String WITH_TAG_SYMBOL = "#";
     private final static String WITHOUT_TAG_SYMBOL = "-#";
+
+    Set<String> words;
 
     Set<String> withArtists;
     Set<String> withoutArtists;
@@ -38,24 +43,33 @@ public class PostQuerySearchDTO implements Serializable {
         String[] queryParams = query.split("\\s+");
 
         return new PostQuerySearchDTO(
+                // Title words
+                extractValuesFromQueryParamsByPattern(queryParams, WORDS_PATTERN),
                 // With artists
-                extractValuesFromQueryParams(queryParams, WITH_ARTIST_SYMBOL),
+                extractValuesFromQueryParamsBySymbols(queryParams, WITH_ARTIST_SYMBOL),
                 // Without artists
-                extractValuesFromQueryParams(queryParams, WITHOUT_ARTIST_SYMBOL),
+                extractValuesFromQueryParamsBySymbols(queryParams, WITHOUT_ARTIST_SYMBOL),
                 // With tags
-                extractValuesFromQueryParams(queryParams, WITH_TAG_SYMBOL),
+                extractValuesFromQueryParamsBySymbols(queryParams, WITH_TAG_SYMBOL),
                 // Without tags
-                extractValuesFromQueryParams(queryParams, WITHOUT_TAG_SYMBOL)
+                extractValuesFromQueryParamsBySymbols(queryParams, WITHOUT_TAG_SYMBOL)
         );
     }
 
-    private static Set<String> extractValuesFromQueryParams(final String[] queryParams, final String symbol) {
+    private static Set<String> extractValuesFromQueryParamsBySymbols(final String[] queryParams, final String symbol) {
         return Arrays.stream(queryParams)
-                /* Filter all params that contain symbol from queryParams at position 0
+                /* Filter all params that contain symbols from queryParams at position 0
                    so params symbols cannot overlap */
                 .filter(param -> param.indexOf(symbol) == 0)
                 // Replace symbol
                 .map(param -> param.replace(symbol, ""))
+                .collect(Collectors.toSet());
+    }
+
+    private static Set<String> extractValuesFromQueryParamsByPattern(final String[] queryParams, final Pattern pattern) {
+        return Arrays.stream(queryParams)
+                /* Filter all params that meet regex pattern */
+                .filter(param -> pattern.matcher(param).matches())
                 .collect(Collectors.toSet());
     }
 }
