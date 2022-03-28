6b4472f4d6c43cbd9053a66ea793f433799c1ee9
Sky <skyterix1991@gmail.com>
2022-03-28 18:30:10 +0200

    Fix build.bat for windows

--- build.bat
+++ build.bat
@@ -4,12 +4,12 @@ set VERSION=0.0.1-alpine
 
 mvn install -Dspring.profiles.active=dev -DskipTests
 
-docker build -t furrifyws-storage-artists:$VERSION ./FurrifyWS-Artists
-docker build -t furrifyws-storage-eureka-server:$VERSION ./FurrifyWS-EurekaServer
-docker build -t furrifyws-storage-gateway:$VERSION ./FurrifyWS-Gateway
-docker build -t furrifyws-storage-posts:$VERSION ./FurrifyWS-Posts
-docker build -t furrifyws-storage-sources:$VERSION ./FurrifyWS-Sources
-docker build -t furrifyws-storage-tags:$VERSION ./FurrifyWS-Tags
+docker build -t furrifyws-storage-artists:%VERSION% ./FurrifyWS-Artists
+docker build -t furrifyws-storage-eureka-server:%VERSION% ./FurrifyWS-EurekaServer
+docker build -t furrifyws-storage-gateway:%VERSION% ./FurrifyWS-Gateway
+docker build -t furrifyws-storage-posts:%VERSION% ./FurrifyWS-Posts
+docker build -t furrifyws-storage-sources:%VERSION% ./FurrifyWS-Sources
+docker build -t furrifyws-storage-tags:%VERSION% ./FurrifyWS-Tags
 
 echo "Images built. You can edit and run docker-compose.yml file."
 
