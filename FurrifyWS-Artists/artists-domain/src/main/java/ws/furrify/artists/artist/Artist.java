255c4a580601f1d75e018503eb5c36dbc11df24e
Sky <skyterix1991@protonmail.com>
2022-05-23 22:47:25 +0200

    Fixed logic once again
    Signed-off-by: Sky <skyterix1991@protonmail.com>

--- FurrifyWS-Artists/artists-domain/src/main/java/ws/furrify/artists/artist/Artist.java
+++ FurrifyWS-Artists/artists-domain/src/main/java/ws/furrify/artists/artist/Artist.java
@@ -111,9 +111,9 @@ class Artist {
         // Verify is preferred nickname is already taken
         boolean isPreferredNicknameTaken =
                 // Is nickname different from original
-                this.preferredNickname.equals(preferredNickname) ||
+                !(this.preferredNickname.equals(preferredNickname) ||
                         // Do nickname is selected as preferred in other artists
-                        artistRepository.existsByOwnerIdAndPreferredNickname(ownerId, preferredNickname.getNickname());
+                        !artistRepository.existsByOwnerIdAndPreferredNickname(ownerId, preferredNickname.getNickname()));
 
         if (isPreferredNicknameTaken) {
             throw new
