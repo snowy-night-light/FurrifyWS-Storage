d3390518ec42ed150cd27653f4f78277e50c50c8
Sky <skyterix1991@protonmail.com>
2022-05-24 11:19:22 +0200

    Flac support concept

--- FurrifyWS-Posts/posts-app/src/main/java/ws/furrify/posts/media/strategy/MediaUploadStrategyUtils.java
+++ FurrifyWS-Posts/posts-app/src/main/java/ws/furrify/posts/media/strategy/MediaUploadStrategyUtils.java
@@ -1,9 +1,15 @@
 package ws.furrify.posts.media.strategy;
 
+import lombok.extern.log4j.Log4j2;
 import net.coobird.thumbnailator.Thumbnails;
+import org.apache.commons.io.IOUtils;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameConverter;
+import org.jaudiotagger.audio.exceptions.CannotWriteException;
+import org.jaudiotagger.audio.flac.FlacTagWriter;
+import org.jaudiotagger.tag.flac.FlacTag;
+import org.jaudiotagger.tag.images.Artwork;
 import ws.furrify.posts.media.MediaExtension;
 import ws.furrify.posts.utils.GifDecoder;
 import ws.furrify.shared.exception.Errors;
@@ -14,6 +20,8 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.Future;
@@ -28,11 +36,26 @@ import static ws.furrify.posts.media.MediaExtension.EXTENSION_GIF;
  *
  * @author sky
  */
+@Log4j2
 public class MediaUploadStrategyUtils {
 
     private final static int PART_OF_VIDEO_TO_THUMBNAIL = 3;
     private final static int TIMOUT_FRAME_EXTRACTION_SECONDS = 10;
 
+    private final static String TEMP_FILE_PREFIX = "furrify_temp_file_";
+
+    /**
+     * Generates thumbnail for file with differnet strategy for different filetypes.
+     * For ex. Video will have extracted frame, Audio will have extracted album cover.
+     * If file does not contain artwork null will be returned.
+     *
+     * @param extension Media extension of source file.
+     * @param width     Width of thumbnail.
+     * @param quality   Quality of thumbnail form 0 to 1.
+     * @param source    Source file input stream.
+     * @return JPG thumbnail input stream or null.
+     * @throws IOException Error has occurred processing the file.
+     */
     public static InputStream generateThumbnail(final MediaExtension extension,
                                                 final int width,
                                                 final float quality,
@@ -54,8 +77,58 @@ public class MediaUploadStrategyUtils {
 
                 yield null;
             }
-            case AUDIO -> null;
+            case AUDIO -> generateThumbnailForImage(
+                    width,
+                    quality,
+                    extractAudioFileAlbumCover(source, extension)
+            );
         };
+    }
+
+    private static InputStream extractAudioFileAlbumCover(final InputStream source, final MediaExtension extension) {
+        ByteArrayOutputStream albumCover = new ByteArrayOutputStream();
+
+        switch (extension) {
+            case EXTENSION_FLAC -> {
+                File tempFile = null;
+                BufferedImage artworkBufferedImage = null;
+
+                try {
+                    // Create temp file for artwork extraction
+                    tempFile = File.createTempFile(TEMP_FILE_PREFIX, null);
+                    tempFile.deleteOnExit();
+
+                    try (FileOutputStream out = new FileOutputStream(tempFile)) {
+                        IOUtils.copy(source, out);
+                    }
+                    FlacTag tag = new FlacTag();
+
+                    FlacTagWriter flacTagWriter = new FlacTagWriter();
+                    flacTagWriter.write(tag, tempFile.toPath());
+
+                    Artwork artwork = tag.getFirstArtwork();
+
+                    artworkBufferedImage = (BufferedImage) artwork.getImage();
+
+                    // Write image to output stream
+                    ImageIO.write(artworkBufferedImage, "png", albumCover);
+                } catch (CannotWriteException | IOException e) {
+                    throw new FileContentIsCorruptedException(Errors.FILE_CONTENT_IS_CORRUPTED.getErrorMessage());
+                } finally {
+                    if (tempFile != null && !tempFile.delete()) {
+                        log.warn("Temp file [filename=" + tempFile.getName() + "] could not be deleted from temp directory.");
+                    }
+                }
+            }
+            // TODO Implement other file formats
+        }
+
+        // If cover not found
+        if (albumCover.size() == 0) {
+            return null;
+        }
+
+        return new ByteArrayInputStream(albumCover.toByteArray());
     }
 
     private static InputStream extractFirstFrameFromGif(final InputStream source) throws IOException {
@@ -93,10 +166,10 @@ public class MediaUploadStrategyUtils {
 
         Future<InputStream> inputStreamFuture = threadPool.submit(() -> {
 
-            try (FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(source)) {
-                frameGrabber.start();
+            try (FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(source);
+                 Java2DFrameConverter converter = new Java2DFrameConverter()) {
 
-                Java2DFrameConverter converter = new Java2DFrameConverter();
+                frameGrabber.start();
                 frameGrabber.setVideoFrameNumber(frameGrabber.getLengthInFrames() / PART_OF_VIDEO_TO_THUMBNAIL);
 
                 Frame frame = frameGrabber.grabImage();
