f834d7f900d030f4b49366a12735a0085fa17642
Sky <skyterix1991@protonmail.com>
2022-08-03 17:22:33 +0200

    URI converter
    Signed-off-by: Sky <skyterix1991@protonmail.com>

---
+++ FurrifyWS-Posts/posts-adapters/src/main/java/ws/furrify/posts/converter/URIAttributeConverterImpl.java
@@ -0,0 +1,9 @@
+package ws.furrify.posts.converter;
+
+import ws.furrify.shared.converter.URIAttributeConverter;
+
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class URIAttributeConverterImpl extends URIAttributeConverter {
+}
